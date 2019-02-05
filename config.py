89d40cde45a6e0f8fdd19055f689466903c3f409
Tim <timcreedon15@gmail.com>
2019-02-05 22:33:05 +0000

    Changed os.environ to os.getenv
    Safer & has support for default configs

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+from os import getenv
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI= os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL', '')
 
 
 class ProductionConfig(Config):
