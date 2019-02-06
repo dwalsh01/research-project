65183e135f163cd7e8ca4999597acc3d34c4e7d3
dwalsh01 <daraghwalsh97@gmail.com>
2019-02-06 14:58:36 +0000

    implemented login with flask-login and postgres

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 import os
-from flask import Flask, send_from_directory, jsonify, request
+from flask import Flask, send_from_directory, jsonify, request, redirect, url_for
 from dotenv import load_dotenv
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from passlib.hash import sha256_crypt
+from flask_login import login_required, LoginManager, login_required, login_user, logout_user, current_user
 
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__, static_folder='research-react/build')
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from models import Users, Education
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return Users.query.get(user_id)
 
 sampleData = [
     {"grant_id": 0, "grant_title": 'Sample title 1'},
     {"grant_id": 1, "grant_title": 'Sample title 2'},
     {"grant_id": 2, "grant_title": 'Sample title 3'}
  ]
+
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -36,12 +48,30 @@ def testing():
 
 @app.route('/insert_user')
 def insert_user():
-    me = Users("moyra", "walsh","staff", "mrs. ", "phd", "12313123", 445, "moyra@gmail.com", "asdaf1")
-    db.session.add(me)
-    db.session.commit()
-    res = Users.query.filter_by(f_name='daragh').first()
-    return str(res.f_name)
+    me = Users("matthew", "walsh", "student", "mr", "ltd", "pbkdf2sha256", "252", "mw11@test.com", "password", "131223")
+    me.saveToDB()
+    return redirect(url_for("serve"))
 
+@app.route('/login_user' , methods=['GET', 'POST'])
+def login():
+    content = request.get_json()
+    user = Users.query.filter_by(email=content['email']).first()
+    if user:
+        if sha256_crypt.verify(content['password'], user.password):
+            login_user(user, remember=True)
+            return 'LOGIN SUCCESS'
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return 'SUCCESSFUL LOGOUT'
+
+@app.route('/current')
+def current():
+    if current_user.is_authenticated:
+        return jsonify(current_user.serialize)
+    return 'NO ONE LOGGED IN'
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,18 +1,62 @@
 from app import db
+from passlib.hash import sha256_crypt
+from flask_login import UserMixin
 
-class Users(db.Model):
+def dump_datetime(value):
+    """Deserialize datetime object into string form for JSON processing."""
+    if value is None:
+        return None
+    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     f_name = db.Column(db.String(25), nullable=False)
     l_name = db.Column(db.String(30), nullable=False)
     job_title = db.Column(db.String(50), nullable=False)
-    prefix = db.Column(db.String(20))
+    prefix = db.Column(db.String(20), nullable=False)
     suffix = db.Column(db.String(20))
     phone = db.Column(db.String(25))
     phone_ext = db.Column(db.Integer)
     email=db.Column(db.String(50), nullable=False, unique=True)
-    orcid=db.Column(db.String(50), nullable=False)
+    password=db.Column(db.String(256))
+    orcid=db.Column(db.String(50))
+    authenticated = db.Column(db.Boolean, default=False)
 
-    def __init__ (self, f_name, l_name, job_title, prefix, suffix, phone, phone_ext, email, orcid):
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.id
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           "f_name": self.f_name,
+           "l_name": self.l_name,
+           "job_title": self.job_title,
+           "prefix": self.prefix,
+           "suffix": self.suffix,
+           "phone": self.phone,
+           "phone_ext": self.phone_ext,
+           "email": self.email,
+           "orcid": self.orcid,
+       }
+
+    def __init__ (self, f_name, l_name, job_title, prefix, suffix, phone, phone_ext, email, password, orcid):
         self.f_name = f_name
         self.l_name = l_name
         self.job_title = job_title
@@ -21,22 +65,73 @@ class Users(db.Model):
         self.phone = phone
         self.phone_ext = phone_ext
         self.email = email
+        self.password = sha256_crypt.encrypt(str(password))
         self.orcid = orcid
 
-    def saveToDB(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def __repr__(self):
-        return "<User: {}>".format(self.f_name)
 
 class Education(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), nullable=False)
     person_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     degree = db.Column(db.String(25), nullable=False)
     field_of_study = db.Column(db.String(30), nullable=False)
     institution = db.Column(db.String(25))
     location = db.Column(db.String(50))
-    year_degree_award = db.Column(db.Integer)
+    year_degree_award = db.Column(db.DateTime)
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Employment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    institution = db.Column(db.String(50))
+    location = db.Column(db.String(100))
+    years = db.Column(db.Float)
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Societies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    society_name = db.Column(db.String(50))
+    membership_type = db.Column(db.Boolean)
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+class Awards(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    year = db.Column(db.Integer)
+    awarding_body = db.Column(db.String(50))
+    award_details = db.Column(db.String(100))
+
+    def saveToDB(self):
+            db.session.add(self)
+            db.session.commit()
+
+class Funding(db.Model):
+    id = db.Column(db.Integer, primary_key= True, unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    funding_amt = db.Column(db.Float)
+    funding_body = db.Column(db.String(50))
+    programme = db.Column(db.String(50))
+    status = db.Column(db.Boolean)
+
+
+class Teams(db.Model):
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    name = db.Column(db.String(50))
+    position = db.Column(db.String(50))
+    primary_attribution = db.Column(db.Integer, db.ForeignKey('funding.id'), nullable=False)
