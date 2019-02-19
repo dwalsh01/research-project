699443a36fefc4bc439d8aeb53b123527cbb2c84
Tim <timcreedon15@gmail.com>
2019-02-19 14:44:22 +0000

    Added proposal test

---
+++ sfi/tests/test_proposal.py
@@ -0,0 +1,27 @@
+from sfi.server.models import ProposalCall
+import json
+
+def test_add_proposal(client, app):
+
+    response = client.post(
+        '/calls/add',
+        data=json.dumps({
+            "deadline_text": "Rolling",
+            "deadline_time": "1 month",
+            "award_amount": "Up to 40,000",
+            "title": "Slick new program",
+            "short_text": "A really nice programme",
+            "text_description": "I'll update this later.",
+            "target_audience": "Researchers",
+            "eligibil_text": "none, really",
+            "duration": "3 days",
+            "report_guidelines": "please look at pdf",
+            "contact": "test@email.com"
+        }),
+        content_type='application/json'
+    )
+
+    print(f'resp: {response}')
+    with app.app_context():
+        query = ProposalCall.query.first()
+        print(f'q: {query}')
