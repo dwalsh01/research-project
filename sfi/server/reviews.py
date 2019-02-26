4ff1be147de6ed86752089a0c3b377f22a59311a
Tim <timcreedon15@gmail.com>
2019-02-26 18:51:05 +0000

    Clarifying the reviews route

--- sfi/server/reviews.py
+++ sfi/server/reviews.py
@@ -6,6 +6,18 @@ bp = Blueprint('proposal', __name__, url_prefix="/reviews")
 '''
 Should list all of the reviews pending
 for the user 'user_id'
+
+Input: "user_id"
+
+What it should do:
+Query the database for the PendingReviews table
+filter for just this user (reviewer_id = user_id)
+
+output:
+JSON of:
+
+the PendingReviews table
+
 '''
 @bp.route('/pending/<int:user_id')
 def pending_reviews(user_id):
