56568c88700b640bb5d8c883e88e8a66f83cc048
darraghphelan <116357743@umail.ucc.ie>
2019-02-26 01:17:54 +0000

    Added tests and mock data for auth and proposals

--- sfi/tests/test_proposal.py
+++ sfi/tests/test_proposal.py
@@ -1,6 +1,7 @@
 from sfi.server.models import ProposalCall
 import json
 import random
+import datetime
 def test_add_proposal(client, app):
     deadline_text = ["Rolling", "1/2/2020", "2/4/2019"]
     award_amount = ["90000", "20000", "30000", "500000", "2000000", "100000"]
@@ -16,10 +17,10 @@ def test_add_proposal(client, app):
     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     "deserunt mollit anim id est laborum."]
     audience = ["Researchers", "Universities", "Scientfic Institutions"]
-    eligibil_text = ["","","",""]
+    eligibil_text = ["deserunt mollit anim id est laborum.","Ut enim ad minim veniam","consectetur adipiscing elit"]
     emails = ["Sed", "eiusmod", "tempor", "incididunt", "labore", "dolore", "magna", "aliqua"]
-
-
+    start_date = ["2019-3-7", "2019-5-1", "2019-6-9"]
+    start_date_end = ["2025-1-2", "2022-11-7", "2021-1-20"]
     num_inserts = 5
     for i in range(num_inserts):
         response = client.post(
@@ -35,6 +36,8 @@ def test_add_proposal(client, app):
                 "eligibil_text": random.choice(eligibil_text),
                 "duration": str(random.randint(1,7)) + "months",
                 "report_guidelines": "Click here for a list of guidelines",
+                "start_date": random.choice(start_date),
+                "start_date_end": random.choice(start_date_end),
                 "contact": random.choice(emails) + str(random.randint(0,999)) + "@email.com"
             }),
             content_type='application/json'
