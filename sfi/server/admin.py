ab12a2f656c08f267708bddcf28e021ab3b8b26e
kyle quinlan <kyleqn@gmail.com>
2019-02-27 17:03:31 +0000

    Added various user_roles with flask-login
    Updated the models to facilitate various roles.
    Updated requirements.txt.
    Restricted nrp routes to admin Role.

--- sfi/server/admin.py
+++ sfi/server/admin.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, request, jsonify
 from .models import NrpSchema, NrpArea
 from .common_functions import post_request_full
+from flask_user import roles_required
 
 bp = Blueprint("admin", __name__, url_prefix="/api")
 
 @bp.route("/nrp", methods=["POST"])
+@roles_required('admin')
 def add_nrp():
     return post_request_full(NrpArea, "NRP added")
 
 @bp.route('/nrp', methods=['GET'])
+@roles_required('admin')
 def list_nrp():
     nrp = NrpArea.query.all()
     schema = NrpSchema(many=True)
