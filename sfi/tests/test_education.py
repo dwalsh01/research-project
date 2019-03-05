506ebc1fe2fb97632d61e72f5bde43ba77ad6a5a
darraghphelan <116357743@umail.ucc.ie>
2019-03-05 13:18:25 +0000

    Added education test and mock data

--- sfi/tests/test_education.py
+++ sfi/tests/test_education.py
@@ -8,32 +8,33 @@ def test_education(client, app):
     institution = ["UCC", "UCD", "TCD", "DCU", "UL", "NUIG", "NUIM"]
     location = ["Cork", "Dublin", "Limerick", "Galway", "Maynooth"]
     year_degree_award = ["2014-1-1", "2010-1-1", "2007-1-1", "2012-1-1"]
-    
-    user_id = None
-    with app.app_context():
-        query = Users.query.all()
-        for user in query:
-            ident = user.id
-            if Education.query.filter_by(user_id=ident).first():
-                continue
-            else:
-                user_id = ident
-                break
+    num_inserts = 5
+    for i in range(num_inserts):
+        user_id = None
+        with app.app_context():
+            query = Users.query.all()
+            for user in query:
+                ident = user.id
+                if Education.query.filter_by(user_id=ident).first():
+                    continue
+                else:
+                    user_id = ident
+                    break
 
-    response = client.post(
-        '/profile/education',
-        data=json.dumps({
-            "user_id": user_id,
-            "degree": random.choice(degree),
-            "field_of_study": random.choice(field_of_study),
-            "institution": random.choice(institution),
-            "location": random.choice(location),
-            "year_degree_award": random.choice(year_degree_award)
-        }),
-        content_type='application/json'
-    )
+        response = client.post(
+            '/profile/education',
+            data=json.dumps({
+                "user_id": user_id,
+                "degree": random.choice(degree),
+                "field_of_study": random.choice(field_of_study),
+                "institution": random.choice(institution),
+                "location": random.choice(location),
+                "year_degree_award": random.choice(year_degree_award)
+            }),
+            content_type='application/json'
+        )
 
     print(f'resp: {response}')
     with app.app_context():
         query = Education.query.all()
-    assert len(query) >= 1
+    assert len(query) >= num_inserts
